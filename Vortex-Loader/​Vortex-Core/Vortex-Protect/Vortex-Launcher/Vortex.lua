7e458dc1296cee9b80a3e6a1705059376f825513
Vortex | X System <kevinisraelcollantes@gmail.com>
2026-07-29 09:39:11 -0600

    Fix URL in loadstring for Vortex.lua

--- Vortex-Loader/​Vortex-Core/Vortex-Protect/Vortex-Launcher/Vortex.lua
+++ Vortex-Loader/​Vortex-Core/Vortex-Protect/Vortex-Launcher/Vortex.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubushttps://raw.githubusercontent.com/vortex-x-system/vortex-x-scripts/refs/heads/main/Vortex-Launcher/Vortex-Loader/%E2%80%8BVortex-Core/Vortex.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vortex-x-system/vortex-x-scripts/refs/heads/main/Vortex-Launcher/Vortex-Loader/%E2%80%8BVortex-Core/Vortex.lua"))()
