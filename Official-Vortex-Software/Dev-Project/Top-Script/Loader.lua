b809ecb89dee9419cf038b6ad31a8eb67fe7e69b
Vortex | X Software <kevinisraelcollantes@gmail.com>
2026-08-25 12:21:36 -0600

    Add Loader script for game scripts management

---
+++ Official-Vortex-Software/Dev-Project/Top-Script/Loader.lua
@@ -0,0 +1,27 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+
+local BASE_URL = "https://raw.githubusercontent.com/Israel-Vortex/vortex-x-scripts/refs/heads/main/Official-Vortex-Software/Vortex-Devs/"
+
+-- Diccionario con los PlaceIds y sus respectivos archivos .lua
+local games = {
+    [93978595733734]  = "V-DISTRICT.lua",
+    [135856908115931] = "DMVSS.lua",
+    [142823291]       = "MM2.lua"
+}
+
+local scriptFile = games[game.PlaceId]
+
+if scriptFile then
+    local success, err = pcall(function()
+        loadstring(game:HttpGet(BASE_URL .. scriptFile))()
+    end)
+    
+    if not success then
+        warn("[VORTEX SOFTWARE] Error al cargar el script:", err)
+    end
+else
+    warn("[VORTEX SOFTWARE] Juego no registrado o script en mantenimiento.")
+end
+
