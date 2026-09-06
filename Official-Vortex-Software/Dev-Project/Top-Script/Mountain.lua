0341db6d89887969d08084562ea53abfb0c2d762
Vortex | X Software <kevinisraelcollantes@gmail.com>
2026-09-06 14:23:59 -0600

    Actualizar Mountain.lua

--- Official-Vortex-Software/Dev-Project/Top-Script/Mountain.lua
+++ Official-Vortex-Software/Dev-Project/Top-Script/Mountain.lua
@@ -248,6 +248,7 @@ local Window = WindUI:CreateWindow({
     IconSize = 35,
     Author = "by ISRAEL CC",
     Folder = "VortexXSoftwareMineAMountain",
+    Background = "rbxassetid://129234239237449",
     Resizable = false,
     HideSearchBar = true,
     Transparent = false,
